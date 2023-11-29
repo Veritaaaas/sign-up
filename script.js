b6a94e061c7aec910e7d8899f7ccd1af86068f75
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2023-11-29 22:13:42 +0800

    added password validation in javascript

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
-first_name = document.getElementById("first_name");
-last_name = document.getElementById("last_name");
-email = document.getElementById("email");
-password = document.getElementById("password");
-confirm_password = document.getElementById("confirm_password");
 
-if (first_name.value == "") 
+var input = document.querySelectorAll("input");
+var password = document.getElementById("password");
+var confirm_password = document.getElementById("confirm-password");
+var button = document.getElementById("submit");
+
+button.addEventListener("click", function(event) {
+
+    if(validatePassword(password.value, confirm_password.value) == false)
+    {
+        confirm_password.style.border = "2px solid red";
+        confirm_password.setCustomValidity("Passwords do not match!");
+        event.preventDefault();
+    }
+    else
+    {
+        confirm_password.style.border = ""; 
+        confirm_password.setCustomValidity("");
+        confirm_password.reportValidity();
+    }
+    confirm_password.reportValidity();
+
+});
+
+function validatePassword(pass1, pass2)
 {
-    first_name.style.border = "2px solid red";
-}
-else
-{
-    first_name.style.border = "2px solid green";
-}
+    console.log(pass2);
+    if(pass1 === pass2)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+} 
 
